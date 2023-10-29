01051f3595d6e0d2ac99e3ccc277bb3e786f948c
Administrator <308205428@qq.com>
2023-10-29 22:30:18 +0800

    1。大屏接口++

---
+++ kxmall-admin-ui/src/api/dashboard/dashboard.js
@@ -0,0 +1,49 @@
+import request from '@/utils/request'
+
+
+// 用户数量统计
+export function countUser(data) {
+  return request({
+    method: 'get',
+    url: '/dashboard/dashboard/countUser',
+  })
+}
+// 销售统计
+export function getSalesStatement(data) {
+  return request({
+    method: 'get',
+    url: '/dashboard/dashboard/getSalesStatement?storageId='+data,
+  })
+}
+
+// 近两日销售统计
+export function getTodayAndYesterdaySales(data) {
+  return request({
+    method: 'get',
+    url: '/dashboard/dashboard/getTodayAndYesterdaySales?storageId='+data,
+  })
+}
+
+// 聚合数据
+export function integral(data) {
+  return request({
+    method: 'get',
+    url: '/dashboard/dashboard/integral?storageId='+data,
+  })
+}
+
+// 按小时统计销量
+export function getSalesByHour(data) {
+  return request({
+    method: 'get',
+    url: '/dashboard/dashboard/getSalesByHour?storageId='+data,
+  })
+}
+
+// 获取仓库
+export function storageList() {
+  return request({
+    url: '/storage/storage/listAll',
+    method: 'get'
+  })
+}
