4f1db5ffcdb52751e833b72f16c112cc27dd001c
308205428@qq.com <308205428@qq.com>
2024-10-20 10:50:15 +0800

    add:keyValue更新

---
+++ kxmall-common/src/main/java/com/kxmall/common/core/IntArrayValuable.java
@@ -0,0 +1,15 @@
+package com.kxmall.common.core;
+
+/**
+ * 可生成 Int 数组的接口
+ *
+ * @author 郅兴
+ */
+public interface IntArrayValuable {
+
+    /**
+     * @return int 数组
+     */
+    int[] array();
+
+}
---
+++ kxmall-common/src/main/java/com/kxmall/common/core/KeyValue.java
@@ -0,0 +1,22 @@
+package com.kxmall.common.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Key Value 的键值对
+ *
+ * @author 郅兴
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeyValue<K, V> implements Serializable {
+
+    private K key;
+    private V value;
+
+}
