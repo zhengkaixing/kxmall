4f53e70a245302afb549be3dd9357b63265678a5
308205428@qq.com <308205428@qq.com>
2024-11-08 14:57:11 +0800

    fix:销量问题修复

--- kxmall-system/src/main/java/com/kxmall/product/mapper/KxStoreProductMapper.java
+++ kxmall-system/src/main/java/com/kxmall/product/mapper/KxStoreProductMapper.java
@@ -46,4 +46,45 @@ public interface KxStoreProductMapper extends BaseMapperPlus<KxStoreProductMappe
      * @return
      */
     Integer decSkuStock(@Param("productId") Long productId, @Param("num") Integer num, @Param("storeId") Long storeId);
+
+    /**
+     * 库存回扣
+     * @param productId
+     * @param num
+     * @param storeId
+     * @return
+     */
+    Integer restoreSkuStock(@Param("productId") Long productId, @Param("num") Integer num, @Param("storeId") Long storeId);
+
+    /**
+     * 增加销量
+     * @param productId
+     * @param num
+     */
+    void incSales(@Param("productId") Long productId, @Param("num") Integer num);
+
+
+    /**
+     * 获取产品通过仓库
+     * @param storageId
+     * @return
+     */
+    List<KxStoreProduct> getProductTitleAllByStorageId(@Param("storageId") Long storageId);
+
+    /**
+     * 秒杀库存扣减
+     * @param productId
+     * @param cartNum
+     * @param storeId
+     * @return
+     */
+    Integer decSeckillStock(@Param("productId") Long productId, @Param("seckillId") Long seckillId, @Param("cartNum") Integer cartNum, @Param("storeId") Long storeId);
+
+    /**
+     * 查询该仓库下的多规格
+     * @param storageId
+     * @param commonId
+     * @return
+     */
+    List<KxStoreProduct> selectListByStorage(@Param("storageId") Long storageId, @Param("commonId") String commonId);
 }
