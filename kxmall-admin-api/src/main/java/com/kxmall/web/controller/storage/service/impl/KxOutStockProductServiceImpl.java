ce4df7cfe3dd9472f7d752a50d6012b89215e330
308205428@qq.com <308205428@qq.com>
2025-02-11 08:17:45 +0800

    update:KxOutStockProductServiceImpl更新

--- kxmall-admin-api/src/main/java/com/kxmall/web/controller/storage/service/impl/KxOutStockProductServiceImpl.java
+++ kxmall-admin-api/src/main/java/com/kxmall/web/controller/storage/service/impl/KxOutStockProductServiceImpl.java
@@ -1,23 +1,23 @@
 package com.kxmall.web.controller.storage.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
-import com.kxmall.common.core.page.TableDataInfo;
-import com.kxmall.common.core.domain.PageQuery;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.kxmall.common.core.domain.PageQuery;
+import com.kxmall.common.core.page.TableDataInfo;
 import com.kxmall.common.utils.StringUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.kxmall.storage.domain.KxOutStockProduct;
 import com.kxmall.storage.domain.bo.KxOutStockProductBo;
 import com.kxmall.storage.domain.vo.KxOutStockProductVo;
-import com.kxmall.storage.domain.KxOutStockProduct;
 import com.kxmall.storage.mapper.KxOutStockProductMapper;
 import com.kxmall.web.controller.storage.service.IKxOutStockProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Collection;
 
 /**
  * 出库商品Service业务层处理
