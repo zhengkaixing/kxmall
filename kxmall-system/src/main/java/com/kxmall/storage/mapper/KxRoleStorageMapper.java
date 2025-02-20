d53aa1de0df2272804f719857301e84139dd9070
308205428@qq.com <308205428@qq.com>
2025-02-20 22:28:29 +0800

    fix:KxRoleStorageMapper

---
+++ kxmall-system/src/main/java/com/kxmall/storage/mapper/KxRoleStorageMapper.java
@@ -0,0 +1,17 @@
+package com.kxmall.storage.mapper;
+
+import com.kxmall.common.core.mapper.BaseMapperPlus;
+import com.kxmall.storage.domain.KxRoleStorage;
+import com.kxmall.storage.domain.vo.RoleStorageVo;
+
+/**
+ * 仓库权限管理Mapper接口
+ *
+ * @author kxmall
+ * @date 2023-08-27
+ */
+public interface KxRoleStorageMapper extends BaseMapperPlus<KxRoleStorageMapper, KxRoleStorage, RoleStorageVo> {
+
+
+
+}
