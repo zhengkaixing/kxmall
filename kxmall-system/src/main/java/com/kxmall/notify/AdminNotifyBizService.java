2fb8eeeaebcba6f11dec7ce5762aa2b93d8879a7
308205428@qq.com <308205428@qq.com>
2025-03-06 08:50:31 +0800

    add：管理端通知类

---
+++ kxmall-system/src/main/java/com/kxmall/notify/AdminNotifyBizService.java
@@ -0,0 +1,19 @@
+package com.kxmall.notify;
+
+import com.kxmall.order.domain.bo.OrderMessageBO;
+import com.kxmall.order.domain.vo.KxStoreOrderVo;
+
+/**
+ * Description: 管理员通知相关接口，意为：发货、退款审核等需要通知管理员即时处理。此业务不应该影响主流业务，不应该抛异常，并且可以异步执行
+ * User: admin
+ * Date: 2019/12/27
+ * Time: 16:15
+ */
+public interface AdminNotifyBizService {
+
+    public void newOrder(KxStoreOrderVo kxStoreOrder);
+
+    public void refundOrder(KxStoreOrderVo kxStoreOrder);
+
+    void newRiderOrder(OrderMessageBO orderMessageBO);
+}
