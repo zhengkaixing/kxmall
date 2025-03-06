84c5ab59cc3fb7cdfcd250b511a418f859edb765
308205428@qq.com <308205428@qq.com>
2025-03-07 07:50:48 +0800

    add：管理端通知类v2

---
+++ kxmall-system/src/main/java/com/kxmall/order/domain/bo/KxProsuctParam.java
@@ -0,0 +1,16 @@
+package com.kxmall.order.domain.bo;
+
+import lombok.Data;
+
+/**
+ * @author :
+ */
+@Data
+public class KxProsuctParam {
+
+    /**
+     * 商品id
+     */
+    private Long productId;
+
+}
