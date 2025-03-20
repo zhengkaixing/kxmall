cc7b3116ba7fefede55c0e54cf8b65e13e8f5a6f
308205428@qq.com <308205428@qq.com>
2025-03-20 08:11:56 +0800

    fix：模板类更新

--- kxmall-common/src/main/java/com/kxmall/common/event/TemplateBean.java
+++ kxmall-common/src/main/java/com/kxmall/common/event/TemplateBean.java
@@ -11,28 +11,18 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TemplateBean {
+
     private String templateType;
     private String orderId;
     private String time;
     private String price;
-    private String deliveryName;
-    private String deliveryId;
-    private String payType;
-    private Long uid;
-    /**
-     * 提现申请ID
-     */
-    private Long extractId;
-
-    private Long employeeSn;
-    private String email;
     private String name;
-    private String companyName;
-    private String sendTime;
-    private String salaryAmt;
-    private String phone;
-    private Long sendId;
+    private Long storeId;
 
 
+
+    private Long riderId;
+    private String address;
+    private String freightPrice;
 
 }
