3904cd0424da68c4c6fdbf9f20ec507016230b31
308205428@qq.com <308205428@qq.com>
2025-03-29 14:03:19 +0800

    add：售后类v3

---
+++ kxmall-system/src/main/java/com/kxmall/group/mapper/KxGroupShopProductMapper.java
@@ -0,0 +1,15 @@
+package com.kxmall.group.mapper;
+
+import com.kxmall.common.core.mapper.BaseMapperPlus;
+import com.kxmall.group.domain.KxGroupShopProduct;
+import com.kxmall.group.domain.vo.KxGroupShopProductVo;
+
+/**
+ * 团购商品Mapper接口
+ *
+ * @author kxmall
+ * @date 2023-10-07
+ */
+public interface KxGroupShopProductMapper extends BaseMapperPlus<KxGroupShopProductMapper, KxGroupShopProduct, KxGroupShopProductVo> {
+
+}
