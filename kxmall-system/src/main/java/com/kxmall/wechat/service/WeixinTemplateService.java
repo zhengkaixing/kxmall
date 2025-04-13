fd5aa075b9bc0d36c8a8b54e10761a8acc838ecd
308205428@qq.com <308205428@qq.com>
2025-04-13 21:14:27 +0800

    update: 微信发送模板

--- kxmall-system/src/main/java/com/kxmall/wechat/service/WeixinTemplateService.java
+++ kxmall-system/src/main/java/com/kxmall/wechat/service/WeixinTemplateService.java
@@ -1,16 +1,18 @@
 
 package com.kxmall.wechat.service;
 
-import cn.hutool.core.util.StrUtil;
-import com.kxmall.common.exception.ServiceException;
+import com.kxmall.storage.domain.KxStorage;
+import com.kxmall.storage.mapper.KxStorageMapper;
 import com.kxmall.wechat.WxMpConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,30 +24,61 @@ import java.util.Map;
 @Service
 public class WeixinTemplateService {
 
+    @Resource
+    private KxStorageMapper storageMapper;
+
     /**
-     * 发送工资
+     * 客户下单，推送给商家
      *
-     * @param phone
-     * @param sendId
+     * @param name
+     * @param sendTime
+     * @param price
+     * @param orderId
+     * @param storeId
      */
-    public void sendSalary(String name, String sendTime, String salaryAmt, String phone, Long sendId) {
+    public void sendPlayOrder(String name, String sendTime, String price, String orderId, Long storeId) {
 
-        String openid = this.getUserOpenid(phone);
-
-        if (StrUtil.isBlank(openid)) {
+        String openid = this.getStoreOpenid(storeId);
+        if (StringUtils.isBlank(openid)) {
             return;
         }
-
         Map<String, String> map = new HashMap<>();
-        map.put("first", "发工资啦！");
         //订单号
-        map.put("keyword1", name);
-        map.put("keyword2", sendTime);
-        map.put("keyword3", salaryAmt);
-        //map.put("remark", ShopConstants.YSHOP_WECHAT_PUSH_REMARK);
-        String tempId = "CirSf1QdfcIYsKzGGeNmCdJhxU8i098Oay64CiI6tRY";
-        if (StrUtil.isNotBlank(tempId)) {
-            this.sendWxMpTemplateMessage(openid, tempId, this.getSiteUrl() + "#/pages/login/verify?sendId=" + sendId, map);
+        map.put("character_string1", orderId);
+        map.put("time2", sendTime);
+        map.put("amount4", price);
+        map.put("thing5", name);
+        String tempId = "OHqVq9tYbHPlN9tAXbnJvJO8hKcTzUXGsUCFfqOOLCk";
+        if (StringUtils.isNotBlank(tempId)) {
+            this.sendWxMpTemplateMessage(openid, tempId, null, map);
+        }
+    }
+
+    /**
+     * 商家点击平台配送，推送给骑手
+     *
+     * @param sendTime
+     * @param price
+     * @param orderId
+     * @param freightPrice
+     * @param riderId
+     */
+    public void sendPlayRiderOrder(String sendTime, String price, String orderId, String address, String freightPrice, Long riderId) {
+
+        String openid = "";
+        if (StringUtils.isBlank(openid)) {
+            return;
+        }
+        Map<String, String> map = new HashMap<>();
+        //订单号
+        map.put("character_string2", orderId);
+        map.put("time5", sendTime);
+        map.put("amount3", price);
+        map.put("thing15", address);
+        map.put("thing11", "配送费：" + freightPrice);
+        String tempId = "p2yVaXF9P7aCMtff8BfBX9XkzibOtlU4ufaR5ubScy8";
+        if (StringUtils.isNotBlank(tempId)) {
+            this.sendWxMpTemplateMessage(openid, tempId, null, map);
         }
     }
 
@@ -60,54 +93,41 @@ public class WeixinTemplateService {
      */
     private String sendWxMpTemplateMessage(String openId, String templateId, String url, Map<String, String> map) {
         WxMpTemplateMessage templateMessage = WxMpTemplateMessage.builder()
-            .toUser(openId)
-            .templateId(templateId)
-            .url(url)
-            .build();
+                .toUser(openId)
+                .templateId(templateId)
+                .url(url)
+                .build();
         map.forEach((k, v) -> {
             templateMessage.addData(new WxMpTemplateData(k, v, "#000000"));
         });
-        String msgId = null;
+        String msgId;
         WxMpService wxService = WxMpConfiguration.getWxMpService();
         try {
             msgId = wxService.getTemplateMsgService().sendTemplateMsg(templateMessage);
         } catch (WxErrorException e) {
-            log.error(e.getMessage(),e);
-            throw new ServiceException(e.getMessage());
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage());
         }
         return msgId;
     }
 
-    /**
-     * 返回H5 url
-     *
-     * @return url
-     */
-    private String getSiteUrl() {
-//        String apiUrl = RedisUtils.getCacheObject(ShopKeyUtils.getSiteUrl());
-//        if (StrUtil.isBlank(apiUrl)) {
-//            return "http://h5.qyygfw.cn/";
-//        }
-//        return apiUrl;
-        return "";
-    }
 
     /**
      * 获取openid
      *
-     * @param phone
+     * @param storeId
      * @return String
      */
-    private String getUserOpenid(String phone) {
-//        SysUserEmployee sysUserEmployee = sysUserEmployeeService.selectUserByPhonenumber(phone);
-//        if (sysUserEmployee == null) {
-//            throw new ServiceException("手机号不存在！");
-//        }
-//        if (StrUtil.isBlank(sysUserEmployee.getOpenId())) {
-//            throw new ServiceException("用户未绑定微信公众号！");
-//        }
-//        return sysUserEmployee.getOpenId();
-        return "";
+    private String getStoreOpenid(Long storeId) {
+        KxStorage kxStorage = storageMapper.selectById(storeId);
+        if (kxStorage == null) {
+            throw new RuntimeException("门店信息不存在！");
+        }
+        if (StringUtils.isBlank(kxStorage.getOpenId())) {
+            throw new RuntimeException("用户未绑定微信公众号！");
+        }
+        return kxStorage.getOpenId();
+
     }
 
 
