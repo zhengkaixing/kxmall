184a0ea001d678741c5549df10a34d9d3a0717de
308205428@qq.com <308205428@qq.com>
2025-05-26 09:17:22 +0800

    add:首页页面模板

---
+++ kxmall-system/src/main/java/com/kxmall/decorate/mapper/KxPageTemplateMapper.java
@@ -0,0 +1,15 @@
+package com.kxmall.decorate.mapper;
+
+import com.kxmall.common.core.mapper.BaseMapperPlus;
+import com.kxmall.decorate.domain.KxPageTemplate;
+import com.kxmall.decorate.domain.vo.KxPageTemplateVo;
+
+/**
+ * 首页页面模板Mapper接口
+ *
+ * @author kxmall
+ * @date 2023-11-05
+ */
+public interface KxPageTemplateMapper extends BaseMapperPlus<KxPageTemplateMapper, KxPageTemplate, KxPageTemplateVo> {
+
+}
