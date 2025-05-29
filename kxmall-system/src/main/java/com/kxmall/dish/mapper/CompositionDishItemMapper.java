b65733125d01c986e924f58d92b2fe4a8925169d
308205428@qq.com <308205428@qq.com>
2025-05-30 07:09:51 +0800

    add:家常菜Mapper

---
+++ kxmall-system/src/main/java/com/kxmall/dish/mapper/CompositionDishItemMapper.java
@@ -0,0 +1,12 @@
+package com.kxmall.dish.mapper;
+
+import com.kxmall.common.core.mapper.BaseMapperPlus;
+import com.kxmall.dish.domain.CompositionDishItemDO;
+import com.kxmall.dish.domain.vo.CompositionDishItemVo;
+
+/**
+ * Generate Code By kxmall
+ */
+public interface CompositionDishItemMapper extends BaseMapperPlus<CompositionDishItemMapper, CompositionDishItemDO, CompositionDishItemVo> {
+
+}
---
+++ kxmall-system/src/main/java/com/kxmall/dish/mapper/CompositionDishMapper.java
@@ -0,0 +1,13 @@
+package com.kxmall.dish.mapper;
+
+
+import com.kxmall.common.core.mapper.BaseMapperPlus;
+import com.kxmall.dish.domain.CompositionDishDO;
+import com.kxmall.dish.domain.bo.CompositionDishDTO;
+
+/**
+ * Generate Code By kxmall
+ */
+public interface CompositionDishMapper extends BaseMapperPlus<CompositionDishMapper, CompositionDishDO, CompositionDishDTO> {
+
+}
