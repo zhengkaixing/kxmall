4aa71aa46b7d627160e3b7e7425b4f89c9412f2d
308205428@qq.com <308205428@qq.com>
2025-06-09 21:44:31 +0800

    add:商品秒杀视图

---
+++ kxmall-system/src/main/java/com/kxmall/seckill/domain/vo/KxStoreSeckillVo.java
@@ -0,0 +1,190 @@
+package com.kxmall.seckill.domain.vo;
+
+import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+
+/**
+ * 商品秒杀视图对象 kx_store_seckill
+ *
+ * @author kxmall
+ * @date 2024-05-15
+ */
+@Data
+@ExcelIgnoreUnannotated
+public class KxStoreSeckillVo {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 商品秒杀产品表id
+     */
+    @ExcelProperty(value = "商品秒杀产品表id")
+    private Long id;
+
+    private Long storageId;
+
+    /**
+     * 商品id
+     */
+    @ExcelProperty(value = "商品id")
+    private Long productId;
+
+    /**
+     * 推荐图
+     */
+    @ExcelProperty(value = "推荐图")
+    private String image;
+
+    /**
+     * 轮播图
+     */
+    @ExcelProperty(value = "轮播图")
+    private String images;
+
+    /**
+     * 活动标题
+     */
+    @ExcelProperty(value = "活动标题")
+    private String title;
+
+    /**
+     * 简介
+     */
+    @ExcelProperty(value = "简介")
+    private String info;
+
+    /**
+     * 价格
+     */
+    @ExcelProperty(value = "价格")
+    private BigDecimal price;
+
+    /**
+     * 成本
+     */
+    @ExcelProperty(value = "成本")
+    private BigDecimal cost;
+
+    /**
+     * 原价
+     */
+    @ExcelProperty(value = "原价")
+    private BigDecimal otPrice;
+
+    /**
+     * 返多少积分
+     */
+    @ExcelProperty(value = "返多少积分")
+    private BigDecimal giveIntegral;
+
+    /**
+     * 排序
+     */
+    @ExcelProperty(value = "排序")
+    private Integer sort;
+
+    /**
+     * 库存
+     */
+    @ExcelProperty(value = "库存")
+    private Integer stock;
+
+    /**
+     * 销量
+     */
+    @ExcelProperty(value = "销量")
+    private Integer sales;
+
+    /**
+     * 单位名
+     */
+    @ExcelProperty(value = "单位名")
+    private String unitName;
+
+    /**
+     * 邮费
+     */
+    @ExcelProperty(value = "邮费")
+    private BigDecimal postage;
+
+    /**
+     * 内容
+     */
+    @ExcelProperty(value = "内容")
+    private String description;
+
+    /**
+     * 开始时间
+     */
+    @ExcelProperty(value = "开始时间")
+    private String startTime;
+
+    /**
+     * 结束时间
+     */
+    @ExcelProperty(value = "结束时间")
+    private String stopTime;
+
+    /**
+     * 产品状态
+     */
+    @ExcelProperty(value = "产品状态")
+    private Integer status;
+
+    /**
+     * 是否包邮
+     */
+    @ExcelProperty(value = "是否包邮")
+    private Integer isPostage;
+
+    /**
+     * 热门推荐
+     */
+    @ExcelProperty(value = "热门推荐")
+    private Integer isHot;
+
+    /**
+     * 最多秒杀几个
+     */
+    @ExcelProperty(value = "最多秒杀几个")
+    private Long num;
+
+    /**
+     * 显示
+     */
+    @ExcelProperty(value = "显示")
+    private Integer isShow;
+
+    /**
+     * 时间段id
+     */
+    @ExcelProperty(value = "时间段id")
+    private Integer timeId;
+
+    /**
+     * 规格 0单 1多
+     */
+    @ExcelProperty(value = "规格 0单 1多")
+    private Integer specType;
+
+    /**
+     * 运费模板id
+     */
+    @ExcelProperty(value = "运费模板id")
+    private Integer tempId;
+
+    /**
+     *
+     */
+    @ExcelProperty(value = "")
+    private Integer isDel;
+
+
+    private String seckillStartTime;
+
+    private String seckillStopTime;
+}
