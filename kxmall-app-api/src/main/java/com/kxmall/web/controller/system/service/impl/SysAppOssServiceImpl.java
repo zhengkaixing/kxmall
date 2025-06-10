2cb4f7b1b16085c6c66599151d9db234c9029ac8
308205428@qq.com <308205428@qq.com>
2025-06-10 20:33:42 +0800

    fix: app图片上传问题处理

--- kxmall-app-api/src/main/java/com/kxmall/web/controller/system/service/impl/SysAppOssServiceImpl.java
+++ kxmall-app-api/src/main/java/com/kxmall/web/controller/system/service/impl/SysAppOssServiceImpl.java
@@ -1,17 +1,28 @@
 package com.kxmall.web.controller.system.service.impl;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.kxmall.common.constant.FileConstants;
 import com.kxmall.common.exception.ServiceException;
 import com.kxmall.common.utils.BeanCopyUtils;
 import com.kxmall.common.utils.StringUtils;
+import com.kxmall.common.utils.io.FilesUtils;
 import com.kxmall.oss.core.OssClient;
+import com.kxmall.oss.domain.SysOssContent;
 import com.kxmall.oss.entity.UploadResult;
 import com.kxmall.oss.enumd.AccessPolicyType;
 import com.kxmall.oss.factory.OssFactory;
+import com.kxmall.oss.mapper.SysOssContentMapper;
+import com.kxmall.oss.properties.OssProperties;
 import com.kxmall.system.domain.SysOss;
+import com.kxmall.system.domain.SysOssConfig;
 import com.kxmall.system.domain.bo.SysOssBo;
+import com.kxmall.system.domain.vo.SysOssConfigVo;
 import com.kxmall.system.domain.vo.SysOssVo;
+import com.kxmall.system.mapper.SysOssConfigMapper;
 import com.kxmall.system.mapper.SysOssMapper;
 import com.kxmall.web.controller.system.service.ISysAppOssService;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +43,10 @@ public class SysAppOssServiceImpl implements ISysAppOssService {
 
     private final SysOssMapper baseMapper;
 
+    private final SysOssConfigMapper sysOssConfigMapper;
+
+    private final SysOssContentMapper ossContentMapper;
+
 
     private LambdaQueryWrapper<SysOss> buildQueryWrapper(SysOssBo bo) {
         Map<String, Object> params = bo.getParams();
@@ -50,6 +65,18 @@ public class SysAppOssServiceImpl implements ISysAppOssService {
 
     @Override
     public SysOssVo upload(MultipartFile file) {
+        //获取当前启动的配置
+        SysOssConfigVo configVo = sysOssConfigMapper.selectVoOne(new LambdaQueryWrapper<SysOssConfig>().eq(SysOssConfig::getStatus, 0));
+        if (FileConstants.DISK.equals(configVo.getConfigKey())) {
+            return disk(file);
+        }else if(FileConstants.DATABASE.equals(configVo.getConfigKey())){
+            return database(file);
+        }else {
+            return getSysOssVo(file);
+        }
+    }
+
+    private SysOssVo getSysOssVo(MultipartFile file) {
         String originalfileName = file.getOriginalFilename();
         String suffix = StringUtils.substring(originalfileName, originalfileName.lastIndexOf("."), originalfileName.length());
         OssClient storage = OssFactory.instance();
@@ -66,6 +93,7 @@ public class SysAppOssServiceImpl implements ISysAppOssService {
         oss.setFileName(uploadResult.getFilename());
         oss.setOriginalName(originalfileName);
         oss.setService(storage.getConfigKey());
+        baseMapper.insert(oss);
         SysOssVo sysOssVo = new SysOssVo();
         BeanCopyUtils.copy(oss, sysOssVo);
         return this.matchingUrl(sysOssVo);
@@ -84,5 +112,89 @@ public class SysAppOssServiceImpl implements ISysAppOssService {
             oss.setUrl(storage.getPrivateUrl(oss.getFileName(), 120));
         }
         return oss;
+    }
+
+    public SysOssVo disk(MultipartFile file) {
+
+        try {
+            String name = file.getOriginalFilename();
+            String suffix = StringUtils.substring(name, name.lastIndexOf("."), name.length());
+            byte[] content = IoUtil.readBytes(file.getInputStream());
+            String path = FilesUtils.generatePath(content, name);
+            // 执行写入
+            String filePath = getFilePath(path);
+            FileUtil.writeBytes(content, filePath);
+            OssProperties config = OssFactory.instanceConfig();
+            // 拼接返回路径
+            String fileUrl = this.formatFileUrl(config.getEndpoint(),FileConstants.DISK, path);
+
+            // 保存文件信息
+            SysOss oss = new SysOss();
+            oss.setUrl(fileUrl);
+            oss.setFileSuffix(suffix);
+            oss.setFileName(name);
+            oss.setOriginalName(name);
+            oss.setService(FileConstants.DISK);
+            baseMapper.insert(oss);
+            SysOssVo sysOssVo = new SysOssVo();
+            BeanCopyUtils.copy(oss, sysOssVo);
+            return sysOssVo;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public SysOssVo database(MultipartFile file) {
+
+        try {
+            String name = file.getOriginalFilename();
+            String suffix = StringUtils.substring(name, name.lastIndexOf("."), name.length());
+            byte[] content = IoUtil.readBytes(file.getInputStream());
+            String path = FilesUtils.generatePath(content, name);
+
+            OssProperties config = OssFactory.instanceConfig();
+
+            SysOssContent ossContent = new SysOssContent();
+            ossContent.setPath(path);
+            ossContent.setConfigKey(FileConstants.DATABASE);
+            ossContent.setContent(content);
+            ossContentMapper.insert(ossContent);
+            // 拼接返回路径
+            String fileUrl = this.formatFileUrl(config.getEndpoint(), FileConstants.DATABASE, path);
+
+            // 保存文件信息
+            SysOss oss = new SysOss();
+            oss.setUrl(fileUrl);
+            oss.setFileSuffix(suffix);
+            oss.setFileName(name);
+            oss.setOriginalName(name);
+            oss.setService(FileConstants.DATABASE);
+            baseMapper.insert(oss);
+            SysOssVo sysOssVo = new SysOssVo();
+            BeanCopyUtils.copy(oss, sysOssVo);
+            return sysOssVo;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getFilePath(String path) {
+        OssProperties ossProperties = OssFactory.instanceConfig();
+        return ossProperties.getDomain() +"/" + path;
+    }
+
+
+    /**
+     * 格式化文件的 URL 访问地址
+     * 使用场景：local、ftp、db，通过 FileController 的 getFile 来获取文件内容
+     *
+     * @param domain 自定义域名
+     * @param path 文件路径
+     * @return URL 访问地址
+     */
+    protected String formatFileUrl(String domain, String type,String path) {
+        return StrUtil.format("{}/system/oss/{}/get/{}", domain, type, path);
     }
 }
