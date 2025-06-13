79771873d80ca89f7f3e41f17a398878409b8f4a
308205428@qq.com <308205428@qq.com>
2025-06-14 07:27:27 +0800

    add:新鲜时报对象mapper

---
+++ kxmall-system/src/main/java/com/kxmall/newtimes/mapper/KxNewTimesMapper.java
@@ -0,0 +1,15 @@
+package com.kxmall.newtimes.mapper;
+
+import com.kxmall.newtimes.domain.KxNewTimes;
+import com.kxmall.newtimes.domain.vo.KxNewTimesVo;
+import com.kxmall.common.core.mapper.BaseMapperPlus;
+
+/**
+ * 新鲜时报Mapper接口
+ *
+ * @author kxmall
+ * @date 2023-10-05
+ */
+public interface KxNewTimesMapper extends BaseMapperPlus<KxNewTimesMapper, KxNewTimes, KxNewTimesVo> {
+
+}
