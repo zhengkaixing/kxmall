a9b787199d4a5dc200712f497f3175ed07b6aee7
308205428@qq.com <308205428@qq.com>
2025-06-30 07:39:13 +0800

    fix:推荐

--- kxmall-app-api/src/main/java/com/kxmall/web/controller/recommend/service/impl/KxAppRecommendService.java
+++ kxmall-app-api/src/main/java/com/kxmall/web/controller/recommend/service/impl/KxAppRecommendService.java
@@ -1,8 +1,10 @@
 package com.kxmall.web.controller.recommend.service.impl;
 
-import cn.hutool.core.collection.CollUtil;
-import com.kxmall.common.core.domain.entity.SysDictData;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.kxmall.carousel.domain.KxCarousel;
+import com.kxmall.carousel.mapper.KxCarouselMapper;
 import com.kxmall.common.core.page.TableDataInfo;
+import com.kxmall.common.enums.StatusType;
 import com.kxmall.common.utils.redis.RedisUtils;
 import com.kxmall.recommend.domain.vo.KxRecommendVo;
 import com.kxmall.recommend.mapper.KxRecommendMapper;
@@ -12,9 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.time.Duration;
 import java.util.List;
-
-import static com.kxmall.common.constant.ProductCacheConstants.RECOMMEND_NAME;
 
 /**
  * @author 郅兴开源团队-小黑
@@ -28,6 +29,10 @@ public class KxAppRecommendService implements IKxAppRecommendService {
     private final KxRecommendMapper baseMapper;
 
     private final SysDictDataMapper dictDataMapper;
+
+    private final KxCarouselMapper carouselMapper;
+
+    private static final String RECOMMEND_NAME = "RECOMMEND_TYPE_";
 
     @Override
     public TableDataInfo<KxRecommendVo> getRecommendByType(Long storageId, Integer recommendType, Integer pageNo, Integer pageSize) {
@@ -44,18 +49,17 @@ public class KxAppRecommendService implements IKxAppRecommendService {
         Long count = baseMapper.getRecommendByStorageCount(storageId, recommendType);
         TableDataInfo<KxRecommendVo> tableDataInfo = new TableDataInfo<>(recommendDTOList, count);
         if (!CollectionUtils.isEmpty(recommendDTOList)) {
-            RedisUtils.setCacheObject(keyCache, tableDataInfo);
+            RedisUtils.setCacheObject(keyCache, tableDataInfo, Duration.ofDays(1));
         }
         return tableDataInfo;
     }
 
     @Override
-    public List<SysDictData> getRecommendTypeEnums() {
-        List<SysDictData> dictDatas = dictDataMapper.selectDictDataByType("recommend_type");
-        dictDatas.sort((me1, me2) -> me2.getDictValue().compareTo(me1.getDictValue()));
-        if (CollUtil.isNotEmpty(dictDatas)) {
-            return dictDatas;
-        }
-        return null;
+    public List<KxCarousel> getRecommendTypeEnums() {
+        LambdaQueryWrapper<KxCarousel> wrapper = new LambdaQueryWrapper<KxCarousel>()
+                .eq(KxCarousel::getStatus, StatusType.ACTIVE.getCode());
+        wrapper.eq(KxCarousel::getAdType, 4);
+        wrapper.orderByDesc(KxCarousel::getSort);
+        return carouselMapper.selectList(wrapper);
     }
 }
