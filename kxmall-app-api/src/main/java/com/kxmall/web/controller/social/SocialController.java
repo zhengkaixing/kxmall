0aa848f25274e3588498df270a9f07e0bb43e9a9
308205428@qq.com <308205428@qq.com>
2025-07-03 06:44:06 +0800

    add:获取模板列表

---
+++ kxmall-app-api/src/main/java/com/kxmall/web/controller/social/SocialController.java
@@ -0,0 +1,33 @@
+package com.kxmall.web.controller.social;
+
+
+import com.kxmall.common.core.domain.R;
+import com.kxmall.wechat.service.MiniTemplateService;
+import lombok.RequiredArgsConstructor;
+import me.chanjar.weixin.common.bean.subscribemsg.TemplateInfo;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/social/app")
+public class SocialController {
+
+    @Resource
+    private MiniTemplateService miniTemplateService;
+
+    /**
+     * 获取模板列表
+     */
+    @GetMapping("/get-subscribe-template-list")
+    public R<List<TemplateInfo>> getSubscribeTemplateList() {
+        return R.ok(miniTemplateService.getSubscribeTemplateList());
+    }
+
+}
