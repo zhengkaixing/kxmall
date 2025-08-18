857495d0826c523a737a6de4bd68c64f8ae127db
308205428@qq.com <308205428@qq.com>
2025-08-18 08:21:40 +0800

    add：添加注释生成接口文档

--- kxmall-system/src/main/java/com/kxmall/dish/domain/CompositionDishItemDO.java
+++ kxmall-system/src/main/java/com/kxmall/dish/domain/CompositionDishItemDO.java
@@ -6,31 +6,56 @@ import com.kxmall.common.core.domain.BaseEntity;
 import lombok.Data;
 
 /**
+ * 组合菜品项实体类
  * Generate Code By kxmall
  */
-
 @Data
 @TableName("kx_composition_dish_item")
 public class CompositionDishItemDO extends BaseEntity {
 
+    /**
+     * 主键ID
+     */
     @TableId(value = "id")
     private Long id;
 
+    /**
+     * 商品ID
+     */
     private Long productId;
+
+    /**
+     * 商品条形码
+     */
     private String barCode;
+
+    /**
+     * 所属菜品ID
+     */
     private Long dishId;
 
-    //商品类目
+    /**
+     * 商品类目名称
+     */
     private String categoryName;
 
+    /**
+     * 商品数量
+     */
     private Long num;
 
+    /**
+     * 单位
+     */
     private String unit;
 
-    //商品名称
+    /**
+     * 商品名称
+     */
     private String productName;
 
-
-    //商品名称
+    /**
+     * 商品属性名称
+     */
     private String productAttrName;
 }
